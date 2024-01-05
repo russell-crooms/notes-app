b1ca4adbd708348e6b4be7d996b88394873518ca
marvelousdre <110522817+marvelousdre@users.noreply.github.com>
2024-01-04 22:55:39 -0600

    Added functionality to add a line break when enter key is pressed

--- script.js
+++ script.js
@@ -35,3 +35,10 @@ notesContainer.addEventListener("click", function(e) {
         })
     }
 }) 
+
+document.addEventListener("keydown", event => {
+    if(event.key === "Enter") {
+        document.execCommand("insertLineBreak"); 
+        event.preventDefault(); 
+    }
+})
